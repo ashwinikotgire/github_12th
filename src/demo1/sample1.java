f2f7cef099e4bd1adffe00f63a5749336e0752d4
hp <hp@DESKTOP-1KMNSM7>
2022-07-18 16:18:44 +0530

    [ashwini] next change in project

---
+++ src/demo1/sample1.java
@@ -0,0 +1,17 @@
+package demo1;
+
+public class sample1 
+{
+
+	
+	public static void main(String[] args)
+	{
+	
+		System.out.println("hiiiiii");
+		System.out.println("hellowwwwwww");
+		
+		
+		
+		
+	}
+}
